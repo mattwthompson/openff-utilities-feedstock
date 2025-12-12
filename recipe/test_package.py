a77e0b76507c79058dbaab3b33db436de85a6e62
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-12-12 08:03:27 -0600

    Fix test

--- recipe/test_package.py
+++ recipe/test_package.py
@@ -2,5 +2,5 @@ from openff.utilities import __version__
 
 from packaging.version import Version
 
-assert Version(__version__) > Version("0.0.0")
-print('{get_ambertools_version()=}')
+assert Version(__version__) > Version("0.0.0"), f"FOUND VERSION {__version__}"
+print("{get_ambertools_version()=}")
